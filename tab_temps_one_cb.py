96e178240e21c7334f7df137033969e999aaf813
Cyprien GINDRE <cyprien.a.gindre@capgemini.com>
2022-06-30 17:38:45 +0200

    vers une seule comboboxunit

--- tab_temps_one_cb.py
+++ tab_temps_one_cb.py
@@ -47,11 +47,44 @@ class tab_temps(QtWidgets.QWidget):
     def on_cell_changed(self):
         print("IN on_cell_changed")
 
+    def on_cell_clicked(self, row, column):
+        print("IN on_cell_clicked")
+
+        # S'il s'agit de la colonne des unités ... Ajout d'une combobox
+        if column == 1:
+            liste_unite = ["s", "mn", "h", "j", "an"]
+            print("type(self.table.item(row, column)) = ", type(self.table.item(row, column)))
+            unit = self.table.item(row, column).text()
+            print("unit = ", unit)
+            self.table.cb_unit = ComboboxUnit()
+            mon_index = liste_unite.index(unit)
+            self.table.cb_unit.setCurrentIndex(mon_index)
+            self.table.cb_unit.currentTextChanged.connect(self.on_cb_convert(row))
+            self.table.setCellWidget(row, 1, self.table.cb_unit)
+
+        if self.previous_row != row:
+            print("cellule différente ! AVANT : ", self.previous_row, " MTN : ", row)
+            #unit = self.table.cb_unit.currentText()
+            #print("unit = ", unit)
+            self.table.removeCellWidget(self.previous_row, 1)
+            self.previous_row = row
+
+
+
+    # def on_item_tab_temps_add_row(self):
+    #     """Fixe une ligne supplémentaire à la fin de tableau temps manuel avec liste déroulante dans unité"""
+    #     nb_rows = self.rowCount()
+    #     self.setRowCount(nb_rows + 1)
+    #     unit_default = QTableWidgetItem("s")
+    #     self.setItem(nb_rows, 1, unit_default)
+
     def __init__(self, parent = None):
         QtWidgets.QWidget.__init__(self, parent)
         self.table = QtWidgets.QTableWidget(3, 2)
         self.table.columnLabels = ["Temps", "Unité"]
         header = self.table.setHorizontalHeaderLabels(self.table.columnLabels)
+
+        self.previous_row = -1
 
         self.list_temps = ListeInstant()
         une_liste_valeurs = [10, 20, 30, 40, 50, 60, 70, 120]
@@ -67,20 +100,17 @@ class tab_temps(QtWidgets.QWidget):
         liste_unite = ["s", "mn", "h", "j", "an"]
         for i in range(nb_rows):
             value = QTableWidgetItem(conversions.scientific_notation(str(self.list_temps.data[i].valeur())))
-            unit = str(self.list_temps.data[i].unite())
+            unit = QTableWidgetItem(str(self.list_temps.data[i].unite()))
 
             self.table.setItem(i, 0, value)
-
-            self.table.cb_unit = ComboboxUnit()
-            mon_index = liste_unite.index(unit)
-            self.table.cb_unit.setCurrentIndex(mon_index)
-            self.table.cb_unit.currentTextChanged.connect(self.on_cb_convert(i))
-            self.table.setCellWidget(i, 1, self.table.cb_unit)
+            self.table.setItem(i, 1, unit)
 
         # Affiche une ligne supplementaire par defaut
-        last_row_nb = self.table.rowCount()
-        self.table.insertRow(last_row_nb)
-        self.table.setCellWidget(last_row_nb, 1, ComboboxUnit())
+        nb_rows = self.table.rowCount()
+        self.table.setRowCount(nb_rows + 1)
+        unit_default = QTableWidgetItem("s")
+        self.table.setItem(nb_rows, 1, unit_default)
+
 
         # Formatage largeur de colonnes
         self.table.setColumnWidth(1, 50)
@@ -88,14 +118,15 @@ class tab_temps(QtWidgets.QWidget):
         self.table.show()
 
         # Detection changement d'item
-        self.table.setMouseTracking(True)
-        self.table.itemChanged.connect(self.on_item_changed)
-        self.table.itemEntered.connect(self.on_item_entered)
-        self.table.currentItemChanged.connect(self.on_current_item_changed)
-        self.table.cellEntered.connect(self.on_cell_entered)
+        # self.table.setMouseTracking(True)
+        # self.table.itemChanged.connect(self.on_item_changed)
+        # self.table.itemEntered.connect(self.on_item_entered)
+        # self.table.currentItemChanged.connect(self.on_current_item_changed)
+        # self.table.cellEntered.connect(self.on_cell_entered)
 
-        self.table.cellChanged.connect(self.on_cell_changed)
-        self.table.currentCellChanged.connect(self.on_current_cell_changed)
+        self.table.cellClicked.connect(self.on_cell_clicked)
+        # self.table.cellChanged.connect(self.on_cell_changed)
+        # self.table.currentCellChanged.connect(self.on_current_cell_changed)
 
         # Detection d'un changement d'unité
         # print(type(self.table.cellWidget(0,1)), type(self.table.cellWidget(0,1)))
